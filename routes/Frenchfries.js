83bbe3e5c8cc17aebdfd267ea40b0259d1fc24d0
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2022-11-02 12:07:18 -0500

    modified frenchfries.js

--- routes/Frenchfries.js
+++ routes/Frenchfries.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Frenchfries', { title: 'Search Results for Frenchfries ' });
+  res.render('Frenchfries', { title: 'Search Results for Frenchfries' });
 });
 
 module.exports = router;
